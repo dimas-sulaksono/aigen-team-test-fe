29ccb1a86cd88d80a0439ddb5afed8da984b4081
dimas sulaksono <sulaksono.work@gmail.com>
2025-03-21 11:54:58 +0700

    fix styling detail button

--- src/components/atoms/Row/index.js
+++ src/components/atoms/Row/index.js
@@ -20,7 +20,7 @@ export const Row = ({ data, setDataDetails, handleShow, handleDownload }) => {
             <button
               onClick={handleClick}
               type="button"
-              class="rounded-full border border-gray-200 bg-white px-3 py-1 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:ring-4 focus:ring-gray-100 focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+              class="rounded-full border border-gray-200 bg-white px-3 py-1 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:ring-4 focus:ring-gray-100 focus:outline-none"
             >
               Details
             </button>
